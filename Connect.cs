1ad5f447bff19fbec180f98e36a410f7c704c6ac
Carlos Quintero <13848706+carl-quintero@users.noreply.github.com>
2025-11-23 13:46:10 +0100

    Initial version

---
+++ Connect.cs
@@ -0,0 +1,31 @@
+﻿using Extensibility;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MyAddInVBA
+{
+   [ProgId("AddInVBA.Connect"), Guid("B68B74CD-71AB-49D2-8B3E-3D3FAAA1935C"), ComVisible(true)]
+   public class Connect : IDTExtensibility2
+   {
+      public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+      {
+      }
+
+      public void OnStartupComplete(ref Array custom)
+      {
+      }
+
+      public void OnAddInsUpdate(ref Array custom)
+      {
+      }
+
+      public void OnBeginShutdown(ref Array custom)
+      {
+      }
+
+      public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+      {
+      }
+
+   }
+}
